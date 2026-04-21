485c7254109c12d2cf698d7fc5ec436ad7e70608
a.sahin <aydin.sahin100@gmail.com>
2026-04-22 01:05:29 +0200

    feat: implemented: embedder, vector_store with unit tests, extended requirements.txt

---
+++ src/retrieval/vector_store.py
@@ -0,0 +1,35 @@
+import os
+from abc import ABC, abstractmethod
+from langchain_core.documents import Document
+from langchain_chroma import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
+
+
+class VectorStoreBase(ABC):
+    # Abstract base Class defining the required interface for any Vector Store implementation
+
+    @abstractmethod
+    def add_documents(self, documents: list[Document]) -> None:
+        pass
+
+    @abstractmethod
+    def similarity_search(self, query: str, k: int) -> list[Document]:
+        pass
+
+
+class ChromaVectorStore(VectorStoreBase):
+
+    def __init__(self, embedder: HuggingFaceEmbeddings, collection_name: str):
+        persist_directory = os.getenv("CHROMA_PERSIST_DIR", "./data/chroma")
+
+        self._store = Chroma(
+            collection_name=collection_name, 
+            embedding_function=embedder,
+            persist_directory=persist_directory
+        )
+    
+    def add_documents(self, documents: list[Document]) -> None:
+        self._store.add_documents(documents)
+    
+    def similarity_search(self, query: str, k: int) -> list[Document]:
+        return self._store.similarity_search(query, k=k)
---
+++ tests/test_embedder.py
@@ -0,0 +1,14 @@
+from src.retrieval.embedder import get_embedder
+
+def test_get_embedder_config(mocker):
+    mock_hf = mocker.patch("src.retrieval.embedder.HuggingFaceEmbeddings")
+
+    embedder = get_embedder()
+
+    assert embedder == mock_hf.return_value
+
+    args, kwargs = mock_hf.call_args
+
+    assert kwargs["model_name"] == "sentence-transformers/all-MiniLM-L6-v2"
+    assert kwargs["model_kwargs"] == {'device': 'cpu'}
+    assert kwargs["encode_kwargs"] == {'normalize_embeddings': True}
